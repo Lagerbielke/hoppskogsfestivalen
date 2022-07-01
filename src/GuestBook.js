27b4ed8fb050b3d48d90374335ea6e1748d02d25
Alexander Lagerbielke <alexander.lagerbielke@aros.se>
2022-07-02 00:47:16 +0200

    Add author

--- src/GuestBook.js
+++ src/GuestBook.js
@@ -4,26 +4,39 @@ import React from 'react'
 
 function GuestBook() {
   const [messages, setMessages] = React.useState([])
-  const [currentMessage, setCurrentMessage] = React.useState([])
+  const [currentMessageContent, setCurrentMessageContent] = React.useState('')
+  const [currentAuthor, setCurrentAuthor] = React.useState('')
 
   const sendMessage = () => {
-    var newMessages = [...messages, currentMessage]
+    const newMessage = {
+      content: currentMessageContent,
+      author: currentAuthor
+    }
+    
+    var newMessages = [...messages, newMessage]
     setMessages(newMessages)
-    setCurrentMessage('')
+    setCurrentMessageContent('')
+    setCurrentAuthor('')
   }
 
   return (
     <div>
       <div>
-        <input type="text"
-          value={currentMessage}
-          onChange={(e) => setCurrentMessage(e.target.value)}
+        <input type="textarea"
+          value={currentMessageContent}
+          onChange={(e) => setCurrentMessageContent(e.target.value)}
         />
-        <button onClick={sendMessage}>Skicka</button>
+        <input type="text"
+          value={currentAuthor}
+          onChange={(e) => setCurrentAuthor(e.target.value)}
+        />
+      <button onClick={sendMessage}>Skicka</button>
       </div>
       <div>
         {messages.map((m) => (
-          <div><p>{m}</p></div>
+          <div key={m.content + m.author}>
+            <p>Av {m.author}: {m.content}</p>
+          </div>
         ))}
       </div>
     </div>
