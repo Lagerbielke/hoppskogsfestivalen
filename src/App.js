0b88f24ba10404b3b64c0918fa3b1b8981c31edb
Filip Kantedal <kantedal@gmail.com>
2022-07-02 11:37:30 +0200

    nice

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-black p-6 flex items-center justify-center sticky top-0 text-white text-xl z-50">
-        <div style={{ fontFamily: "'Noto Sans', sans-serif" }}>
-          Hoppskogsfestivalen 2022
+        <div className="flex flex-col justify-center items-center">
+          <div
+            className="font-extrabold font-base italic"
+            style={{ fontFamily: "'Noto Sans', sans-serif" }}
+          >
+            Hoppskogsfestivalen 2022
+          </div>
         </div>
       </header>
       <div className="flex">
