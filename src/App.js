5d2d34e79426dc4e3d42afe7cf42d5811a7b8f6f
Filip Kantedal <kantedal@gmail.com>
2022-07-02 11:06:50 +0200

    wow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import "./App.css";
-import logo from "./logo.svg";
+// import bg from "./assets/nor24.jpeg";
+import edge_left from "./assets/edge_left.png";
+import edge_right from "./assets/edge_right1.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-black p-6 flex items-center justify-center sticky top-0 text-white text-3xl z-50">
+        Hoppskogsfestivalen 2022
       </header>
+      <div className="flex">
+        <div
+          className="bg-repeat-y relative flex-none w-[60px] min-h-screen"
+          style={{
+            backgroundImage: `url(${edge_left})`,
+            backgroundSize: "100% auto",
+            backgroundPosition: "center top",
+          }}
+        />
+        <div className="flex-1" />
+        <div
+          className="bg-repeat-y relative flex-none w-[60px] min-h-screen"
+          style={{
+            backgroundImage: `url(${edge_right})`,
+            backgroundSize: "100% auto",
+            backgroundPosition: "center top",
+          }}
+        />
+      </div>
+      {/* <div
+        className="flex items-center justify-center flex-col bg-black bg-opacity-50 py-48 bg-center bg-cover"
+        style={{
+          backgroundImage: ` url(${bg})`,
+          backgroundBlendMode: "darken",
+        }}
+      >
+        <div className="w-full max-w-3xl mx-auto flex flex-col px-6">
+          <div className="text-white font-bold text-xl lg:text-4xl mb-2">
+            Välkommen till
+          </div>
+          <h1>Hoppskogsfestivalen 2022</h1>
+          <div className="text-white font-bold text-xl lg:text-4xl mt-2">
+            2 juli 2022
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
